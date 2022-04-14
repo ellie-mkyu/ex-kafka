29ffa742b896191420803734d307800a597736e2
ellie <ellie@mkcreative.co.kr>
2022-04-14 18:16:15 +0900

    Initial commit

---
+++ src/main/java/com/test/exkafka/ExKafkaApplication.java
@@ -0,0 +1,13 @@
+package com.test.exkafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExKafkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExKafkaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/test/exkafka/ExKafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.exkafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExKafkaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
